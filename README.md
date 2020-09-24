741e7f495c12ad3d3d9699491ea12279d2513445
yashpal-nimesh <yashpal.n@solutelabs.com>
2020-09-24 19:10:36 +0530

    some changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # graphql-practice
+
+Steps To start
+
+1. cd server npm i npm start
+2. cd client npm i npm start
--- client/src/Components/AddBooks.js
+++ client/src/Components/AddBooks.js
@@ -34,7 +34,6 @@ function AddBook(props) {
     }
 
 
-    <br />
 
     if (props.getAuthorQuery.loading) {
         return (
@@ -52,6 +51,7 @@ function AddBook(props) {
                 <br />
 
     Add New Book
+
                 <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={submitForm}>
 
                     <div class="mb-4">
